fa23db4795115be97b9298660cff7e40ce057459
Barak Rosen <barrozz@gmail.com>
2024-04-03 13:17:23 +0300

    add data for eks cluster

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -42,6 +42,10 @@ provider "aws" {
   }
 }
 
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+}
+
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
